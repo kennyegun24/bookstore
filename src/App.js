d9c0494eae53e7574b780f50df55b1133489e59e
kenny Egun <kennyegun240@gmail.com>
2022-11-10 19:40:50 +0100

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Books from './components/book';
 import Categories from './components/categories';
-import './App.css'
+import './App.css';
 
 const App = () => (
-<>
-<Navbar />
+  <>
+    <Navbar />
 
-<div className='center'>
-  <div>
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/category" element={<Categories />} />
-    </Routes>
-    </div>
+    <div className="center">
+      <div>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/category" element={<Categories />} />
+        </Routes>
+      </div>
     </div>
   </>
-)
+);
 
 export default App;
