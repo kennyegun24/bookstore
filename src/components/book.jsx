d030bcbf8880161fd9a4a18ef9295dc8656c052a
kenny Egun <kennyegun240@gmail.com>
2022-11-15 19:22:25 +0100

    Change id and key format, Imported useEffect hook

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './home';
 import Form from './form';
+import { getBooks } from '../redux/book/book';
 
 const Books = () => {
+  const dispatch = useDispatch();
   const books = useSelector((state) => state.books);
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
   return (
     <>
       {books.map((book) => (
-        <Book key={book.id} id={book.id} title={book.title} author={book.author} />
+        <Book key={book.item_id} id={book.item_id} title={book.title} author={book.author} />
       ))}
       <Form />
     </>
