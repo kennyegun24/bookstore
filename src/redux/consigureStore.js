0a7da5d63a9a4f31837ade88030027969f253746
kenny Egun <kennyegun240@gmail.com>
2022-11-15 19:25:05 +0100

    Link APIs and added functionalities to add, get and delete items

--- src/redux/consigureStore.js
+++ src/redux/consigureStore.js
@@ -1,4 +1,5 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import bookReducer from './book/book';
 import categories from './categories/categories';
 
@@ -7,6 +8,6 @@ const allReducers = combineReducers({
   categories,
 });
 
-const store = configureStore({ reducer: allReducers });
+const store = configureStore({ reducer: allReducers }, applyMiddleware(thunk));
 
 export default store;
